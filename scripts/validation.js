c6fcc99abec3d76a813a4994812b009aec76e586
orzina <orstromm@gmail.com>
2023-10-23 19:19:51 +0300

    Update config object syntax

--- scripts/validation.js
+++ scripts/validation.js
@@ -3,7 +3,7 @@ function showInputError(formElement, inputElement, options) {
     `#${inputElement.id}-error`
   );
   errorMessageElement.textContent = inputElement.validationMessage;
-  errorMessageElement.classList.add(options.inputErrorClass);
+  errorMessageElement.classList.add(options.errorClass);
 }
 
 function hideInputError(formElement, inputElement, options) {
@@ -11,7 +11,7 @@ function hideInputError(formElement, inputElement, options) {
     `#${inputElement.id}-error`
   );
   errorMessageElement.textContent = "";
-  errorMessageElement.classList.remove(options.inputErrorClass);
+  errorMessageElement.classList.remove(options.errorClass);
 }
 
 function checkInputValidity(formElement, inputElement, options) {
@@ -74,8 +74,8 @@ const config = {
   inputSelector: ".modal__form-input",
   submitButtonSelector: ".modal__button",
   inactiveButtonClass: "modal__button_type_inactive",
-  inputErrorClass: "modal__input-error",
-  errorClass: "popup__error_visible",
+  inputErrorClass: "modal__form-input:invalid",
+  errorClass: "modal__input-error",
 };
 
 enableValidation(config);
